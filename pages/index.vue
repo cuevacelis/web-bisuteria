2ead9b2800b56d3d562fa993fbd6f89772e0684c
Jose Cueva Celis <cuevacelis@hotmail.com>
2020-04-24 19:57:11 -0500

    Ultimo Cambio

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        web-bisuteria xd
+        Web-bisuteria
       </h1>
       <h2 class="subtitle">
         Proyecto de una pagina Web de Bisuteria.
