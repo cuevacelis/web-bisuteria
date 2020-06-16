600144446426363b4b76c2b78b886c24c822858c
Jose Cueva Celis <cuevacelis@hotmail.com>
2020-06-15 21:15:31 -0500

    change button

--- pages/index.vue
+++ pages/index.vue
@@ -47,7 +47,7 @@
     </section>
 
     <section class="contenido_texto_principal p-8">
-      <section class="banner_informativo px-4 pb-6 flex justify-center">
+      <section class="banner_informativo flex justify-center">
         <div class="bg-teal-100  border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
           <div class="flex">
             <div class="py-1">
