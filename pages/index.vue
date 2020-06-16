2afc8a98bc221966b7950d873fdabd7fa236e3aa
Jose Cueva Celis <cuevacelis@hotmail.com>
2020-06-15 22:09:10 -0500

    add collares

--- pages/index.vue
+++ pages/index.vue
@@ -89,17 +89,66 @@
               </p>
             </div>
           </section>
+          <section class="producto_inicio sm:w-1/2 md:w-1/3 xl:w-1/4">
+            <div class="contenido-producto p-2 m-2 bg-white shadow-lg">
+              <img
+                class="rounded-md"
+                src="https://scontent.ftru2-3.fna.fbcdn.net/v/t1.0-9/12931187_873954479400051_8645130464251015395_n.jpg?_nc_cat=101&_nc_sid=8bfeb9&_nc_eui2=AeGw5Hp_VExEFGVwl3_MORixExKKVRv-fS8TEopVG_59L9Ccgrm2Osdzqus2o9pGiTL_L_b3jn6jytRRiib6Mu5X&_nc_ohc=PcHwXho9veUAX8i4p6r&_nc_ht=scontent.ftru2-3.fna&oh=adeb2e75eab1aa97a7b415c56cbcde13&oe=5EFF73E9"
+                alt="anillo de trebol"
+                loading="lazy"
+              >
+              <h2 class="titulo_imagen">
+                Collares
+              </h2>
+              <p class="precio">
+                $50
+              </p>
+            </div>
+          </section>
+          <section class="producto_inicio sm:w-1/2 md:w-1/3 xl:w-1/4">
+            <div class="contenido-producto p-2 m-2 bg-white shadow-lg">
+              <img
+                class="rounded-md"
+                src="https://scontent.ftru2-3.fna.fbcdn.net/v/t1.0-9/12931187_873954479400051_8645130464251015395_n.jpg?_nc_cat=101&_nc_sid=8bfeb9&_nc_eui2=AeGw5Hp_VExEFGVwl3_MORixExKKVRv-fS8TEopVG_59L9Ccgrm2Osdzqus2o9pGiTL_L_b3jn6jytRRiib6Mu5X&_nc_ohc=PcHwXho9veUAX8i4p6r&_nc_ht=scontent.ftru2-3.fna&oh=adeb2e75eab1aa97a7b415c56cbcde13&oe=5EFF73E9"
+                alt="anillo de trebol"
+                loading="lazy"
+              >
+              <h2 class="titulo_imagen">
+                Collares
+              </h2>
+              <p class="precio">
+                $50
+              </p>
+            </div>
+          </section>
+          <section class="producto_inicio sm:w-1/2 md:w-1/3 xl:w-1/4">
+            <div class="contenido-producto p-2 m-2 bg-white shadow-lg">
+              <img
+                class="rounded-md"
+                src="https://scontent.ftru2-3.fna.fbcdn.net/v/t1.0-9/12931187_873954479400051_8645130464251015395_n.jpg?_nc_cat=101&_nc_sid=8bfeb9&_nc_eui2=AeGw5Hp_VExEFGVwl3_MORixExKKVRv-fS8TEopVG_59L9Ccgrm2Osdzqus2o9pGiTL_L_b3jn6jytRRiib6Mu5X&_nc_ohc=PcHwXho9veUAX8i4p6r&_nc_ht=scontent.ftru2-3.fna&oh=adeb2e75eab1aa97a7b415c56cbcde13&oe=5EFF73E9"
+                alt="anillo de trebol"
+                loading="lazy"
+              >
+              <h2 class="titulo_imagen">
+                Collares
+              </h2>
+              <p class="precio">
+                $50
+              </p>
+            </div>
+          </section>
         </section>
       </section>
 
       <div class="sm:flex flex-none item-center justify-center bg-gray-500 text-white text-sm font-bold px-4 py-3 m-3" role="alert">
-        <svg class="fill-current w-4 h-4 mt-2 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
-        <p class="mr-4 mt-2">Mira todos los productos que tenemos para ti</p>
+        <svg class="fill-current w-4 h-4 mt-2 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" /></svg>
+        <p class="mr-4 mt-2">
+          Mira todos los productos que tenemos para ti
+        </p>
         <button class="bg-black hover:bg-black text-white font-bold py-2 px-4 rounded-full">
           Click Aqui
         </button>
       </div>
-
     </section>
   </section>
 </template>
