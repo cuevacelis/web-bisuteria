63b7c8b1783bec060b403b2b1e155a73074ade75
Jose Cueva Celis <cuevacelis@hotmail.com>
2020-06-15 21:06:14 -0500

    change index

--- pages/index.vue
+++ pages/index.vue
@@ -73,12 +73,12 @@
         </div>
       </section>
 
-      <section class="algunos-productos ">
+      <section class="collares">
         <section class="titulo">
-          <h1>Algunos Productos</h1>
+          <h1>Collares</h1>
         </section>
         <section class="productos_inicio flex flex-wrap">
-          <section class="producto sm:w-1/2 md:w-1/3 xl:w-1/4">
+          <section class="producto_inicio sm:w-1/2 md:w-1/3 xl:w-1/4">
             <div class="contenido-producto p-2 m-2 rounded-md bg-white shadow-lg">
               <img
                 class="rounded-md"
@@ -94,7 +94,7 @@
               </p>
             </div>
           </section>
-          <section class="producto sm:w-1/2 md:w-1/3 xl:w-1/4">
+          <section class="producto_inicio sm:w-1/2 md:w-1/3 xl:w-1/4">
             <div class="contenido-producto p-2 m-2 rounded-md bg-white shadow-lg">
               <img
                 class="rounded-md"
@@ -110,7 +110,7 @@
               </p>
             </div>
           </section>
-          <section class="producto sm:w-1/2 md:w-1/3 xl:w-1/4">
+          <section class="producto_inicio sm:w-1/2 md:w-1/3 xl:w-1/4">
             <div class="contenido-producto p-2 m-2 rounded-md bg-white shadow-lg">
               <img
                 class="rounded-md"
@@ -126,71 +126,7 @@
               </p>
             </div>
           </section>
-          <section class="producto sm:w-1/2 md:w-1/3 xl:w-1/4">
-            <div class="contenido-producto p-2 m-2 rounded-md bg-white shadow-lg">
-              <img
-                class="rounded-md"
-                src="https://scontent.ftru2-3.fna.fbcdn.net/v/t1.0-9/12931187_873954479400051_8645130464251015395_n.jpg?_nc_cat=101&_nc_sid=8bfeb9&_nc_eui2=AeGw5Hp_VExEFGVwl3_MORixExKKVRv-fS8TEopVG_59L9Ccgrm2Osdzqus2o9pGiTL_L_b3jn6jytRRiib6Mu5X&_nc_ohc=PcHwXho9veUAX8i4p6r&_nc_ht=scontent.ftru2-3.fna&oh=adeb2e75eab1aa97a7b415c56cbcde13&oe=5EFF73E9"
-                alt="anillo de trebol"
-                loading="lazy"
-              >
-              <h2 class="titulo_imagen">
-                Collares
-              </h2>
-              <p class="precio">
-                $50
-              </p>
-            </div>
-          </section>
-          <section class="producto sm:w-1/2 md:w-1/3 xl:w-1/4">
-            <div class="contenido-producto p-2 m-2 rounded-md bg-white shadow-lg">
-              <img
-                class="rounded-md"
-                src="https://scontent.ftru2-3.fna.fbcdn.net/v/t1.0-9/12931187_873954479400051_8645130464251015395_n.jpg?_nc_cat=101&_nc_sid=8bfeb9&_nc_eui2=AeGw5Hp_VExEFGVwl3_MORixExKKVRv-fS8TEopVG_59L9Ccgrm2Osdzqus2o9pGiTL_L_b3jn6jytRRiib6Mu5X&_nc_ohc=PcHwXho9veUAX8i4p6r&_nc_ht=scontent.ftru2-3.fna&oh=adeb2e75eab1aa97a7b415c56cbcde13&oe=5EFF73E9"
-                alt="anillo de trebol"
-                loading="lazy"
-              >
-              <h2 class="titulo_imagen">
-                Collares
-              </h2>
-              <p class="precio">
-                $50
-              </p>
-            </div>
-          </section>
-          <section class="producto sm:w-1/2 md:w-1/3 xl:w-1/4">
-            <div class="contenido-producto p-2 m-2 rounded-md bg-white shadow-lg">
-              <img
-                class="rounded-md"
-                src="https://scontent.ftru2-3.fna.fbcdn.net/v/t1.0-9/12931187_873954479400051_8645130464251015395_n.jpg?_nc_cat=101&_nc_sid=8bfeb9&_nc_eui2=AeGw5Hp_VExEFGVwl3_MORixExKKVRv-fS8TEopVG_59L9Ccgrm2Osdzqus2o9pGiTL_L_b3jn6jytRRiib6Mu5X&_nc_ohc=PcHwXho9veUAX8i4p6r&_nc_ht=scontent.ftru2-3.fna&oh=adeb2e75eab1aa97a7b415c56cbcde13&oe=5EFF73E9"
-                alt="anillo de trebol"
-                loading="lazy"
-              >
-              <h2 class="titulo_imagen">
-                Collares
-              </h2>
-              <p class="precio">
-                $50
-              </p>
-            </div>
-          </section>
-          <section class="producto sm:w-1/2 md:w-1/3 xl:w-1/4">
-            <div class="contenido-producto p-2 m-2 rounded-md bg-white shadow-lg">
-              <img
-                class="rounded-md"
-                src="https://scontent.ftru2-3.fna.fbcdn.net/v/t1.0-9/12931187_873954479400051_8645130464251015395_n.jpg?_nc_cat=101&_nc_sid=8bfeb9&_nc_eui2=AeGw5Hp_VExEFGVwl3_MORixExKKVRv-fS8TEopVG_59L9Ccgrm2Osdzqus2o9pGiTL_L_b3jn6jytRRiib6Mu5X&_nc_ohc=PcHwXho9veUAX8i4p6r&_nc_ht=scontent.ftru2-3.fna&oh=adeb2e75eab1aa97a7b415c56cbcde13&oe=5EFF73E9"
-                alt="anillo de trebol"
-                loading="lazy"
-              >
-              <h2 class="titulo_imagen">
-                Collares
-              </h2>
-              <p class="precio">
-                $50
-              </p>
-            </div>
-          </section>
-          <section class="producto sm:w-1/2 md:w-1/3 xl:w-1/4">
+          <section class="producto_inicio sm:w-1/2 md:w-1/3 xl:w-1/4">
             <div class="contenido-producto p-2 m-2 rounded-md bg-white shadow-lg">
               <img
                 class="rounded-md"
