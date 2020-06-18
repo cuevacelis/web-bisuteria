4eaa173ea38bfce2896317f63e55f50c9bfec1a8
Jose Cueva Celis <cuevacelis@hotmail.com>
2020-06-18 15:46:45 -0500

    cambios en pagina en construcción

--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,16 @@
     </section>
 
     <section class="contenido_texto_principal p-8">
+      <section class="aviso_pagina_en_construccion">
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+          <strong class="font-bold">Alerta!</strong>
+          <span class="block sm:inline">Página en construcción.</span>
+          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+          </span>
+        </div>
+      </section>
+
       <section class="banner_informativo flex justify-center">
         <div class="bg-blue-100 border-t-4 border-blue-600 rounded-b text-teal-900 px-2 py-3 shadow-md" role="alert">
           <div class="flex">
@@ -39,16 +49,6 @@
               </p>
             </div>
           </div>
-        </div>
-      </section>
-
-      <section class="aviso_pagina_en_construccion">
-        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-          <strong class="font-bold">Alerta!</strong>
-          <span class="block sm:inline">Pagina en construcción.</span>
-          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
-          </span>
         </div>
       </section>
 
